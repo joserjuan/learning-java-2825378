2f64a3027502581d3257799923d5b74053554818
joserjuan <joserjuan@gmail.com>
2020-12-27 11:12:30 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 public class Main {
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double listedMealPrice,
+                                               double tipRate,
+                                               double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate *listedMealPrice; 
+        double result = listeMealPrice + tip + tax; 
+        System.out.println("Your total meal price is " + result);
     }
 
     public static void main(String[] args) {
+        calculateTotalMealPrice(15, .2, .08);
     }
 
 }
